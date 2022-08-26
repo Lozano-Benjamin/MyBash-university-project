ab7e4671c97910052a31a3485e2d9d3054c42459
gastonBon <gastonbonfils@gmail.com>
2022-08-26 16:37:31 -0300

    aaaaaaa

--- command.c
+++ command.c
@@ -1,2 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
 #include "command.h"
 #include <glib.h>
+
+scommand scommand_new(void) {
+    return NULL;
+}
+/*
+ * Nuevo `scommand', sin comandos o argumentos y los redirectores vacíos
+ *   Returns: nuevo comando simple sin ninguna cadena y redirectores vacíos.
+ * Ensures: result != NULL && scommand_is_empty (result) &&
+ *  scommand_get_redir_in (result) == NULL &&
+ *  scommand_get_redir_out (result) == NULL
+ */
