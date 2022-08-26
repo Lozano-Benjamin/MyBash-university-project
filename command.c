75f057584e658c837bfc3ead7a456bb7a2f62d42
Fabri <fabriziolongh77@gmail.com>
2022-08-26 17:20:49 -0300

    perdon

--- command.c
+++ command.c
@@ -1,11 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
 #include "command.h"
 #include <glib.h>
 
 
-// hola gastoncito
+struct scommand_s {
+    GSList* comm_args;
+    char* out;
+    char* in;
+};
 
-scommand scommand_new(void){
+scommand scommand_new(void) {
+/*
+ * Nuevo `scommand', sin comandos o argumentos y los redirectores vacíos
+ *   Returns: nuevo comando simple sin ninguna cadena y redirectores vacíos.
+ * Ensures: result != NULL && scommand_is_empty (result) &&
+ *  scommand_get_redir_in (result) == NULL &&
+ *  scommand_get_redir_out (result) == NULL
+ */
 
+    scommand self = malloc(sizeof(struct scommand_s));
+    self->comm_args = NULL;
+    self->out = NULL;
+    self->in = NULL;
 
+    assert(self != NULL && scommand_is_empty (self) &&
+        scommand_get_redir_in (self) == NULL &&
+        scommand_get_redir_out (self) == NULL);
+
+    return self;
 }
+
+
+
+
+
+
 
