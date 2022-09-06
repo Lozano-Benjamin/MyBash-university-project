28cf870b680e523089f3d72c0ecedf0524ef7f53
Benjamin Lozano <benjamin.lozano@mi.unc.edu.ar>
2022-09-05 23:46:01 -0300

    Quick Fix and push

--- parsing.c
+++ parsing.c
@@ -10,39 +10,40 @@ static scommand parse_scommand(Parser p) {
     scommand new_command = scommand_new();
     arg_kind_t type = ARG_NORMAL;
     char *aux = parser_next_argument(p, &type);
-    bool flag = false; 
-    while ( !parser_at_eof(p) && aux != NULL) {
-        if (type == ARG_NORMAL) {
-            if (aux == NULL) {
+   bool flag = false; 
+    while ( type==ARG_NORMAL && !parser_at_eof(p) && aux != NULL ) {
+                if (aux == NULL) {
                 flag = true;
-            }
-            scommand_push_back(new_command, aux);
-            aux = parser_next_argument(p, &type);
-        }
+                }
+                scommand_push_back(new_command, aux);
+                aux = parser_next_argument(p, &type);
     }
+
     for (int i = 0; i<2; i++) {
         if (type == ARG_INPUT) {
-            if (aux == NULL) {
-                flag = true;
-                break;
-            }
-            scommand_set_redir_in(new_command, aux);
-            aux = parser_next_argument(p, &type);
+                if (aux == NULL) {
+                    flag = true;
+                    break;
+                }
+                scommand_set_redir_in(new_command, aux);
+                aux = parser_next_argument(p, &type);
         }else if (type == ARG_OUTPUT) {
-            if (aux == NULL) {
-                flag = true;
-                break;
-            }
-            scommand_set_redir_out(new_command, aux);
-            aux = parser_next_argument(p, &type);
+                if (aux == NULL) {
+                    flag = true;
+                    break;
+                }
+                scommand_set_redir_out(new_command, aux);
+                aux = parser_next_argument(p, &type);
         }
     }
+
     if (flag || scommand_is_empty(new_command)) {
         new_command = scommand_destroy(new_command);
         new_command = NULL;
     }
+
     return new_command; 
-}     
+}
 
 pipeline parse_pipeline(Parser p) {
     pipeline result = pipeline_new();
